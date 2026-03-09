6981ca4bec9ffcec34566c3afba00919956959c6
farhanz200825 <farhanz200825@gmail.com>
2026-03-09 11:08:09 +0700

    menambah fe login dan register

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('auts.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auts.register');
+})->name('register'); // Tambahkan nama route
+
+Route::get('/dashboard', function () {
     return view('welcome');
-});
+})->name('dashboard');
